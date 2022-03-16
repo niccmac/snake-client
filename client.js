fe372545ed8aad316ecfbb08b1c78b29948d0ba8
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-16 12:36:33 -0700

    added name

---
+++ client.js
@@ -0,0 +1,32 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: 50541 // PORT number here,
+
+    
+  });
+ 
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log(`Successfully connected to game server`);
+  });
+
+  conn.on('data', (data) => {
+    console.log(`server returned:`, data);
+  });
+
+  conn.write('Name: NCM');
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = {
+  connect
+};
